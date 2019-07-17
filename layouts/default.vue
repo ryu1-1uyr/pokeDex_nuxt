53d75737e80fa59300cb2c065b307c7b375da5f1
s17001 <s17001@std.it-college.ac.jp>
2019-07-17 11:19:09 +0900

    コンポーネントに切り分けた

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,22 @@
 <template>
   <div>
+    <Header />
     <nuxt />
   </div>
 </template>
+
+<script>
+  import Header from '@/components/header'
+  export default {
+    components: {
+      Header,
+    },
+    data() {
+      return {
+      }
+    },
+  }
+</script>
 
 <style>
 html {
--- pages/index.vue
+++ pages/index.vue
@@ -37,7 +37,7 @@ export default {
 <style>
 .container {
   margin: 0 auto;
-  min-height: 100vh;
+  padding-top: 30px;
   display: flex;
   justify-content: center;
   align-items: center;

a044ddf6a6dd1ae2e43226c8ec72c695882b4383
s17001 <s17001@std.it-college.ac.jp>
2019-07-17 13:42:48 +0900

    使うであろうデータを生やした

--- pages/pokemon/_dex.vue
+++ pages/pokemon/_dex.vue
@@ -1,13 +1,51 @@
 <template>
     <div>
-      くわしいぺーじ
+      <p>くわしいぺーじ</p>
+
+      <Skills />
+      <DefaultData />
+      <Effort />
+      <Graph />
+
     </div>
 </template>
 
 <script>
-    export default {
-        name: "_dex"
+  import Skills from '@/components/pokeSkills'
+  import DefaultData from '@/components/pokeDefaultData'
+  import Effort from '@/components/pokeEffortData'
+  import Graph from '@/components/pokeGraph'
+
+  export default {
+    components: {
+      Skills,
+      DefaultData,
+      Effort,
+      Graph
+    },
+        name: "_dex",
+    data () {
+      return {
+        pokeName: '',
+        pokeNumber: '',
+        pokeAbility: '',
+        pokeIndividualValue: {
+          HP: '',
+          Attack: '',
+          Defense: '',
+          SpecialAttack: '',
+          SpecialDefense: '',
+          Speed: ''
+        }
+
+      }
+    },
+    mounted(){
+      this.pokeNumber = $nuxt.$route.params.dex
+      console.log($nuxt.$route.params.dex)
     }
+
+  }
 </script>
 
 <style scoped>

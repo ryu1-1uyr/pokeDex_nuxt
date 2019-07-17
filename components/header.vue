b1d8852b49888681055b54e513ab6751b6e36f89
s17001 <s17001@std.it-college.ac.jp>
2019-07-17 11:42:13 +0900

    header周りの方針を決めた

--- components/header.vue
+++ components/header.vue
@@ -2,6 +2,9 @@
   <div>
     <b-navbar toggleable="lg" type="dark">
 
+      <b-navbar-toggle target="nav-collapse" style="background-color: #47494e">三</b-navbar-toggle>
+
+
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
           <b-nav-item >
@@ -16,24 +19,26 @@
 
           </b-nav-item>
 
-        </b-navbar-nav>
+        </b-navbar-nav>gs
 
-        <!-- Right aligned nav items -->
+        <!--ここでコンポーネント切り分けるとCSS周りがクソだるくなるのでやめた-->
         <b-navbar-nav class="ml-auto">
           <b-nav-form>
             <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
             <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
           </b-nav-form>
-
         </b-navbar-nav>
+
       </b-collapse>
     </b-navbar>
   </div>
 </template>
 
 <script>
+  // import Search from '@/components/search'
+
     export default {
-        name: "header"
+      name: "header",
     }
 </script>
 

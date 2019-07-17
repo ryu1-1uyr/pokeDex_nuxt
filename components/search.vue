8bc559aba4ec8f9058582495c35e6098386f628d
s17001 <s17001@std.it-college.ac.jp>
2019-07-17 11:41:44 +0900

    make search components

---
+++ components/search.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <b-navbar-nav class="ml-auto">
+      <b-nav-form>
+        <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+        <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+      </b-nav-form>
+    </b-navbar-nav>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "search"
+    }
+</script>
+
+<style scoped>
+
+</style>
